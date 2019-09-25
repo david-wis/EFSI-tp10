0a8283e9f714c2cf765a55ef33c1937287fe4fbd
Android Studio <devandroid@ort>
2019-09-25 09:04:40 -0300

    controller y dao en proceso

--- api/controller/productoController.php
+++ api/controller/productoController.php
@@ -1,30 +1,41 @@
 <?php
-    include_once('../dao/productoDao.php');
+    require_once('./dao/productoDao.php');
     $action = isset($_POST['action']) ? $_POST['action'] : $_GET['action'];
     switch ($action) {
         case 'agregar':
-            $producto = new Producto();
-            $producto->nombre = $_POST['nombre'];
-            $producto->descripcion = $_POST['descripcion'];
-            $producto->imagen = $_POST['imagen'];
-            $producto->precio = $_POST['precio'];
-            $producto->stock = $_POST['stock'];
+            /*  ProductoController no tiene relacion con 
+             *  la clase producto, por lo tanto usamos un array asociativo
+             */
+            $producto = array(); 
+            $producto["nombre"] = $_POST['nombre'];
+            $producto["descripcion"] = $_POST['descripcion'];
+            $producto["imagen"] = $_POST['imagen'];
+            $producto["precio"] = $_POST['precio'];
+            $producto["stock"] = $_POST['stock'];
+            ProductoDao::AgregarProducto($producto);
             break;
         case 'modificar':
-            $producto = new Producto();
-            $producto->nombre = $_POST['nombre'];
-            $producto->descripcion = $_POST['descripcion'];
-            $producto->imagen = $_POST['imagen'];
-            $producto->precio = $_POST['precio'];
-            $producto->stock = $_POST['stock'];
+            $producto = array();
+            $producto["nombre"] = $_POST['nombre'];
+            $producto["descripcion"] = $_POST['descripcion'];
+            $producto["imagen"] = $_POST['imagen'];
+            $producto["precio"] = $_POST['precio'];
+            $producto["stock"] = $_POST['stock'];
+            ProductoDao::ModificarProducto($producto);
             break;
         case 'eliminar':
-            $producto = new Producto();
-            $producto->nombre = $_POST['nombre'];
+            ProductoDao::EliminarProducto($_POST['nombre']);
             break;
-        
+        case 'obtenerTodos':
+            $productos = ProductoDao::ObtenerProductos();
+            echo json_encode($productos);
+            break;
+        case 'obtenerUno':
+            $producto = ProductoDao::ObtenerProducto($_POST['nombre']);
+            echo json_encode($producto);
         default:
-            # code...
+            $respuesta["error"] = "Comando invalido";
+            echo json_encode($respuesta);
             break;
     }
 ?>
--- api/dao/productoDao.php
+++ api/dao/productoDao.php
@@ -2,16 +2,52 @@
     include_once('../model/producto.php');
     include_once('../db.php');
     class ProductoDao {
-        public static AgregarProducto(){
+        public static function AgregarProducto($producto){
+            $result = true;
+            $productoNuevo = new Producto();
+            $result = $result && Validar($productoNuevo->setNombre, $producto['nombre']); 
+            $result = $result && Validar($productoNuevo->setDescripcion, $producto['descripcion']);
+            $result = $result && Validar($productoNuevo->setImagen, $producto['imagen']);
+            $result = $result && Validar($productoNuevo->setPrecio, $producto['precio']);
+            $result = $result && Validar($productoNuevo->setStock, $producto['stock']);
+            return $result;       
+        }
+
+        private static function Validar(&$setter, $valor) {
+            $exito = false;
+            if (isset($valor)) {
+                if (is_string($valor)) { //Nombre - descripcion
+                    if (trim($valor) != ""){
+                        $setter($valor);
+                        $exito = true;
+                    }
+                } else if (is_numeric($valor)) { //Stock - precio
+                    if ($valor >= 0) {
+                        $setter($valor);
+                        $exito = true;
+                    }
+                } else { // Imagen (blob)
+                    $setter($valor);
+                    $exito = true;
+                }
+            }
+            return $exito;
+        }
+
+        public static function ModificarProducto($producto){
 
         }
 
-        public static ModificarProducto(){
-
-        }
-
-        public static EliminarProducto(){
+        public static function EliminarProducto($nombre){
             
+        }
+
+        public static function ObtenerProducto($nombre) {
+
+        }
+
+        public static function ObtenerTodos() {
+
         }
     }
 ?>
